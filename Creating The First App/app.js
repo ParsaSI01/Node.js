8b5fd943a9bebfc3877f0ea6618c39e889d311ad
ParsaSI <parsasi0101@gmail.com>
2026-08-29 14:20:01 +0330

    Creating The First App

---
+++ Creating The First App/app.js
@@ -0,0 +1,16 @@
+const mission = 'Learn Node.js';
+
+if (mission === 'Learn Node.js') {
+    console.log('First Node.js Script');
+} else {
+    console.log('Well I Guess I Gotta Learn Vue.js Then');
+}
+
+console.log(process);
+
+// We Can Give Args Using "node NAME ARGS"
+
+
+const args = process.argv[2];
+
+console.log(args);
---
+++ README.md
@@ -0,0 +1,3 @@
+A simple Node.js project created while learning and practicing Node.js.
+
+This repository contains my Node.js exercises, examples, and experiments as I work through the language.
