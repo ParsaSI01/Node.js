8e28d8cf381277297c00d9c5ced98ac98980b8ee
ParsaSI <parsasi0101@gmail.com>
2026-09-16 00:16:14 +0330

    Using nodemon

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 const axios = require("axios")
 
 axios.get('https://www.google.com').then((res)=>console.log(res))
+
+
+// We Can Use npm run dev To Run Our nodemon Script
+
+// Or We Can Do This: node_modules\.bin\nodemon app.js
