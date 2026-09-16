6d451be0d312e8431ec1a993538da95c78bd7a3d
ParsaSI <parsasi0101@gmail.com>
2026-09-16 16:02:48 +0330

    Route Params In Express

---
+++ Route Params In Express/server.js
@@ -0,0 +1,53 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = 3000;
+
+const users = [
+    {
+        id: 0,
+        firstName: 'John',
+        lastName: 'Doe',
+    },
+    {
+        id: 1,
+        firstName: 'Jane',
+        lastName: 'Doe',
+    },
+    {
+        id: 2,
+        firstName: 'Jack',
+        lastName: 'Doe',
+    },
+    {
+        id: 3,
+        firstName: 'Jim',
+        lastName: 'Doe',
+    },
+];
+
+app.get('/', (req, res) => {
+    res.send('This Is The Home Page');
+});
+
+app.get('/users', (req, res) => {
+    res.send(users);
+});
+
+app.get('/users/:userId', (req, res) => {
+    const userId = req.params.userId;
+    const user = users[userId];
+
+    if (user) {
+        res.json(user);
+    } else {
+        res.status(404).json({
+            error: 'User Does Not Exist',
+        });
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Listening From PORT ${PORT}`);
+});
